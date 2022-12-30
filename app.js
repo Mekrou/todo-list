8d97470466f63a4a35023f5782e3f8f8de3cb38d
mekrm <mills.ryan.011@gmail.com>
2022-12-30 22:07:09 +0000

    filled out logic behind cases

--- app.js
+++ app.js
@@ -1,23 +1,38 @@
 // Simple browser-based 'command-line' todo list.
 
 let quit = false;
-
+let todoList = [];
 
 while (!quit) {
-    let input = prompt("What would you like to get done today?");
+    let input = prompt("\n'new' - add a todo\n 'list' - list all todos\n'delete' - delete a todo\n'quit' - end app");
     input = input.toLowerCase();
     // initial input must be 'new' 'list' 'quit' 'delete' or 'quit'
 
     switch (input)
     {
         case 'new':
-            afdsa
+            {
+                input = prompt("What's up?").toLowerCase();
+                todoList.push(input);
+                break;
+            }
         case 'list':
-            SVGFEDropShadowElement
+            {
+                for (let i = 0; i < todoList.length; i++)
+                {
+                    console.log(`${i}: ${todoList[i]}`);
+                }
+                break;
+            }
         case 'delete':
-            cdsafdsa
+            {
+                input = parseInt(prompt("What is the index of the task you want to remove?"));
+                todoList.splice(input, 1);
+                break;
+            }
         case 'quit':
             quit = true;
+            break;
         default:
             console.log("Invalid input");
     }
