b19273b3b76f9ef191458f056c8d4aa7e2d65e59
mekrm <mills.ryan.011@gmail.com>
2022-12-30 21:56:26 +0000

    init commit

---
+++ app.js
@@ -0,0 +1,10 @@
+// Simple browser-based 'command-line' todo list.
+
+let quit = false;
+let input = prompt("What would you like to get done today?");
+
+while (!quit) {
+    
+    
+    if (input.toLowerCase() === 'q') quit = true;
+}
