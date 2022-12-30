84bbb0aadd3a10e4f30adcaa2c8fd16025738712
mekrm <mills.ryan.011@gmail.com>
2022-12-30 21:57:02 +0000

    switch logic

--- app.js
+++ app.js
@@ -1,10 +1,24 @@
 // Simple browser-based 'command-line' todo list.
 
 let quit = false;
-let input = prompt("What would you like to get done today?");
+
 
 while (!quit) {
-    
-    
-    if (input.toLowerCase() === 'q') quit = true;
+    let input = prompt("What would you like to get done today?");
+    input = input.toLowerCase();
+    // initial input must be 'new' 'list' 'quit' 'delete' or 'quit'
+
+    switch (input)
+    {
+        case 'new':
+            afdsa
+        case 'list':
+            SVGFEDropShadowElement
+        case 'delete':
+            cdsafdsa
+        case 'quit':
+            quit = true;
+        default:
+            console.log("Invalid input");
+    }
 }
